2bad6224b4a8d027fc327a2befdc0b3234c07fa5
lky1020 <57024183+lky1020@users.noreply.github.com>
2020-09-06 16:36:37 +0800

    Update to Generic Type

--- P8/P8Q4/src/InsertionSort.java
+++ P8/P8Q4/src/InsertionSort.java
@@ -2,7 +2,7 @@
 public class InsertionSort {
     
     public static void main(String[] args){
-        int[] arr = new int[]{8, 2, 6, 4, 9, 7, 1};
+        Integer[] arr = new Integer[]{8, 2, 6, 4, 9, 7, 1};
         
         arr = insertionSort(arr);
         
@@ -11,21 +11,21 @@ public class InsertionSort {
         }
     }
     
-    public static int[] insertionSort(int[] arr){
+    public static <T extends Comparable<? super T>> T[] insertionSort(T[] arr){
         int length = arr.length;
         
         for(int i = 1; i < length; i++){
-            int firstUnsorted = arr[i];
+            T firstUnsorted = arr[i];
             insertInOrder(firstUnsorted, arr, i - 1);
         }
         
         return arr;
     }
     
-    public static void insertInOrder(int element, int[] arr, int end){
+    public static <T extends Comparable<? super T>> void insertInOrder(T element, T[] arr, int end){
         int index = end;
         
-        while((index >= 0) && (element < arr[index])){
+        while((index >= 0) && (element.compareTo(arr[index]) <= -1)){
             arr[index + 1] = arr[index];
             index--; //minus index because previous got + 1
         }
