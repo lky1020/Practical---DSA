b32245f51faf91248664849a5dec04b1a6c099ee
lky1020 <57024183+lky1020@users.noreply.github.com>
2020-09-06 16:42:35 +0800

    Delete return for array

--- P8/P8Q4/src/InsertionSort.java
+++ P8/P8Q4/src/InsertionSort.java
@@ -4,14 +4,14 @@ public class InsertionSort {
     public static void main(String[] args){
         Integer[] arr = new Integer[]{8, 2, 6, 4, 9, 7, 1};
         
-        arr = insertionSort(arr);
+        insertionSort(arr);
         
         for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
     }
     
-    public static <T extends Comparable<T>> T[] insertionSort(T[] arr){
+    public static <T extends Comparable<T>> void insertionSort(T[] arr){
         int length = arr.length;
         
         for(int i = 1; i < length; i++){
@@ -19,7 +19,6 @@ public class InsertionSort {
             insertInOrder(firstUnsorted, arr, i - 1);
         }
         
-        return arr;
     }
     
     public static <T extends Comparable<? super T>> void insertInOrder(T element, T[] arr, int end){
